8a0368c1fcb394973cfbebded100e90f3aab9dd2
inmanol <innmanol888@gmail.com>
2020-11-09 08:54:55 +0100

    Segunda update

---
+++ app/src/main/java/com/example/practicapaginesgrogues/cartelera.java
@@ -0,0 +1,14 @@
+package com.example.practicapaginesgrogues;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cartelera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartelera);
+    }
+}
