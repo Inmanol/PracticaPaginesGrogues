65f0a2c181a74e73ae2931333ca3380bdabd6b39
inmanol <innmanol888@gmail.com>
2020-12-03 09:20:36 +0100

    16 update

--- app/src/main/java/com/example/practicapaginesgrogues/carteleraCienciaFiccion.java
+++ app/src/main/java/com/example/practicapaginesgrogues/carteleraCienciaFiccion.java
@@ -3,12 +3,20 @@ package com.example.practicapaginesgrogues;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class carteleraCienciaFiccion extends AppCompatActivity {
+public class carteleraCienciaFiccion extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cartelera_ciencia_ficcion);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
